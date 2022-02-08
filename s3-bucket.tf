1ca68512816fe080644d1024540a72dbd5a9cd9c
Emily Hansen <ehansen@fastly.com>
2022-02-08 16:19:28 +0000

    Only use bucket_prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 module "s3_bucket" {
   source  = "app.terraform.io/ehansen-fastly/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket        = "my-s3-bucket"
   bucket_prefix =  var.prefix 
   acl           = "private"
 }
